e58c879f9d37aa417e5759a55fd01f4d95cf35b4
Lxkas <alohasunshineday@yahoo.com>
2024-04-03 23:24:46 +0700

    feat: dockerize

--- src/lib/setup.ts
+++ src/lib/setup.ts
@@ -6,13 +6,15 @@ import "@sapphire/plugin-logger/register";
 import { setup } from "@skyra/env-utilities";
 import * as colorette from "colorette";
 import { join } from "node:path";
-import { srcDir } from "./constants";
+import { rootDir, srcDir } from "./constants";
 
 // Set default behavior to bulk overwrite
 ApplicationCommandRegistries.setDefaultBehaviorWhenNotIdentical(RegisterBehavior.BulkOverwrite);
 
+const isProduction = process.env.NODE_ENV === "production";
+
 // Read env var
-setup({ path: join(srcDir, ".env") });
+setup({ path: isProduction ? join(rootDir, ".env") : join(srcDir, ".env") });
 
 // Enable colorette
 colorette.createColors({ useColor: true });
